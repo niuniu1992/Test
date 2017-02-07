afb5f98f1d69dbc7df872e99e4375cdd6061190a
Niujp <13120216218@163.com>
2017-02-07 14:33:12 +0800

    hh

---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by MacBook Pro on 2017/2/7.
+//  Copyright © 2017年 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by MacBook Pro on 2017/2/7.
+//  Copyright © 2017年 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
